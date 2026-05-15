5bce8045187dde62c6688b1d2ee1de64fb482b4c
inderbindra <inderbindra@gmail.com>
2026-05-15 19:21:02 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left=0, right = nums.Length - 1, mid=0;
+
+        while(left <= right)
+        {
+            mid = left + (right - left)/2;
+            if(nums[mid] ==  target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
